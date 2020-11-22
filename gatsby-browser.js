81709827d84e6741e1c55a05d04d28cd732de1e8
Pascal Mathys <pascal.mathys@rootix.ch>
2020-11-22 14:59:48 +0100

    chore: update dependencies (#4)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 import './styles/markdown.css';
 import './styles/prism-nord.css';
-import 'tailwindcss/base.css';
-import 'tailwindcss/components.css';
-import 'tailwindcss/utilities.css';
+import 'tailwindcss/dist/base.css';
+import 'tailwindcss/dist/components.css';
+import 'tailwindcss/dist/utilities.css';
 import 'typeface-roboto';
 import 'typeface-roboto-slab';
+
